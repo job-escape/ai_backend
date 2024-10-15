b966d1b4682b85dae3a56b7412b7457f7cbcfd3b
M <myrza.dywc@gmail.com>
2024-10-15 13:21:51 +0500

    check build

--- main/views.py
+++ main/views.py
@@ -259,3 +259,4 @@ class AgentViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
             By default, assumes AgentType of TEXT.
         """
         return super().list(request, *args, **kwargs)
+
